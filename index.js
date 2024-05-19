c7fb9da80f55491b00e1f46837053a5a9d911e85
Eddy <137077017+eddy531@users.noreply.github.com>
2024-05-19 15:22:06 -0500

    Cambio 9

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,6 +34,6 @@ productform.addEventListener("submit", async(e)=>{
     fechaLanza: productform.fechaLanza.value,
     garantia: productform.garantia.value,
     cantidad: productform.cantidad.value
-  })
+  });
   productform.reset();
-})
+});
