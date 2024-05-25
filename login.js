c46c61bdf1f3940d9c11feb23b04d03dd3c247db
eddy531 <eddysopalo85@gmail.com>
2024-05-25 12:50:30 -0500

    Cambio

--- login.js
+++ login.js
@@ -25,7 +25,7 @@ googleLogin.addEventListener("click", function(){
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const user = result.user;
     console.log(user);
-    window.location.href = "Computadoras/index.html";
+    window.location.href = "eddy531.github.io/Computadoras/";
 
     }).catch((error) => {
     const errorCode = error.code;
